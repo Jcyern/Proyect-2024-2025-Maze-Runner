5e0676f0f68f4309afb7a82b4c96460281e39a20
Jcyern <juancarlosyernoespinosa@gmail.com>
2024-11-04 01:30:10 -0500

    art_work

--- Assets/Scripts/File.cs
+++ Assets/Scripts/File.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class File : MonoBehaviour
 {
-    public string Name { get; set; }
-    public string Habilidad { get; set; }
-    public int Enfriamieto{get;set;}
-    public int Velocidad{get;set;}
+    public string Name;
+    public string Habilidad;
+    public int Enfriamieto;
+    public int Velocidad;
+
+    public TextMeshProUGUI nombre;
+    public TextMeshProUGUI habilidad;
+
+    public TextMeshProUGUI enfriamiento;
+
+
+    public void LoadInterface()
+    {
+        nombre.text = Name;
+        habilidad.text = Habilidad;
+        enfriamiento.text = Enfriamieto.ToString();
+    }
 
 }
