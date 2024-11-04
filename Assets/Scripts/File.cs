187eddd4ea2e3e0a518edc8e2d48e6a674335347
Jcyern <juancarlosyernoespinosa@gmail.com>
2024-11-03 23:45:31 -0500

    creating project in unity

---
+++ Assets/Scripts/File.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class File : MonoBehaviour
+{
+    public string Name { get; set; }
+    public string Habilidad { get; set; }
+    public int Enfriamieto{get;set;}
+    public int Velocidad{get;set;}
+
+}
--- obj/Debug/net8.0/Scripts.AssemblyInfo.cs
+++ obj/Debug/net8.0/Scripts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scripts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51792f2c1a2cf537e1bc669528dad824006d77f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scripts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scripts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
